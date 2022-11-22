4465a168dca1a5f82f3709d9cd8004909f2e2ad4
prixhd <manap.aminov@gmail.com>
2022-11-22 23:43:14 +0300

    writing tg bot

--- parsnew.py
+++ parsnew.py
@@ -1,5 +1,4 @@
 import re
-
 from bs4 import BeautifulSoup as bs
 import requests
 import lxml
@@ -7,6 +6,8 @@ import json
 from datetime import datetime
 
 url = "https://nbareplay.net/category/watch-nba-replay/"
+
+print(datetime.now())
 
 matchs_dict = {}
 
@@ -21,7 +22,6 @@ current_month_text = a[current_month % 12]
 
 
 def get_links(urls):
-
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
              (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
@@ -29,15 +29,10 @@ def get_links(urls):
 
     req = requests.get(url, headers)
 
-    with open("data/projects.html", "w") as file:
-        file.write(req.text)
-
-    with open("data/projects.html") as file:
-        site = file.read()
-
-    soup = bs(site, "lxml")
+    soup = bs(req.text, "lxml")
     try:
-        matchs_today = soup.find('main', id="main").find_all('h2', text=re.compile(f'{current_day} {current_month_text} {current_year}'))
+        matchs_today = soup.find('main', id="main").find_all('h2', text=re.compile(
+            f'{current_day} {current_month_text} {current_year}'))
 
         for match in matchs_today:
             match_id = match.find_parent('article').get('id')[-5:]
@@ -51,43 +46,45 @@ def get_links(urls):
 
             req = requests.get(match_link, header)
 
-            with open("data/projects_2.html", "w") as file:
-                file.write(req.text)
+            soup_2 = bs(req.text, "lxml")
 
-            with open("data/projects_2.html") as file:
-                site_2 = file.read()
-
-            soup_2 = bs(site_2, "lxml")
-
-            parts = soup_2.find('div', class_='entry-content').find('strong', text=re.compile('FG by 4 Parts')).find_parent().find_all('a')
-            part = [parts[0].get('href'), parts[1].get('href'), parts[2].get('href'), parts[3].get('href')]
+            parts = soup_2.find('div', class_='entry-content').find('strong', text=re.compile(
+                'FG by 4 Parts')).find_parent().find_all('a')
+            part = []
+            part_name = []
+            for i in parts:
+                if i.get('href') is None:
+                    continue
+                else:
+                    part_name.append(i.find('span').text[1:])
+                    part.append(i.get('href'))
 
             headers_1 = {
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
                  Chrome/107.0.0.0 Safari/537.36"
             }
+
             links = []
+
+            matchs_dict[match_id] = {
+                "match_title": match_title
+            }
+
             for i in part:
                 req = requests.get(i, headers_1)
 
-                with open("data/projects_3.html", "w") as file:
-                    file.write(req.text)
-
-                with open("data/projects_3.html") as file:
-                    sites = file.read()
-
-                soups = bs(sites, "lxml")
+                soups = bs(req.text, "lxml")
 
                 link = 'https:' + soups.find('div', class_='entry-content').find('iframe').get('src')
                 links.append(link)
 
-            matchs_dict[match_id] = {
-                "match_title": match_title,
-                "first_quarter": links[0],
-                "second_quarter": links[1],
-                "third_quarter": links[2],
-                "fourth_quarter": links[3]
-            }
+            part_name_reversed = list(reversed(part_name))
+            links_reversed = list(reversed(links))
+            s = len(links)
+            while s > 0:
+                data = ({f'{part_name_reversed[s-1]}': links_reversed[s-1]})
+                matchs_dict[match_id].update(data)
+                s -= 1
 
             with open("matchs_dict.json", "w") as file:
                 json.dump(matchs_dict, file, indent=4, ensure_ascii=False)
@@ -97,4 +94,4 @@ def get_links(urls):
 
 if __name__ == "__main__":
     get_links(url)
-
+    print(datetime.now())
